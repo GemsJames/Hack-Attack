04f03fc35c1e41fd3524d3c9ce250c59c96ae83a
Unknown <diogo_dpereira@hotmail.com>
2017-04-10 17:57:50 +0100

    1 ScreenState Part1
    New - GameScreen.cs , ScreenManager.cs , SplachScreen.cs.

---
+++ Hack Attack/Hack Attack/GameScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace Hack_Attack
+{
+    public class GameScreen
+    {
+        public virtual void Initialize() { }
+        public virtual void LoadContent(ContentManager Content) { }
+        public virtual void Update(GameTime gameTime) { }
+        public virtual void Draw(SpriteBatch spriteBatch) { }
+    }
+}
---
+++ Hack Attack/Hack Attack/ScreenManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace Hack_Attack
+{
+    public class ScreenManager
+    {
+        #region Variables
+
+        // Guarda os GameScreens
+
+        Dictionary<string, GameScreen> screens = new Dictionary<string, GameScreen>();
+
+        //Screen Stack
+
+        Stack<GameScreen> screenStack = new Stack<GameScreen>();
+
+        //Tamanho do Ecrã
+
+        Vector2 dimensions;
+        #endregion
+    }
+}
