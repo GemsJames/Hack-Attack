d53b62b0e7fa68140c3efd651c3e7609d0e14cd1
Unknown <diogo_dpereira@hotmail.com>
2017-04-24 14:02:26 +0100

    4 e 5 Animation
    New - FadeAnimation.cs , Animation.cs

---
+++ Hack Attack/Hack Attack/Part2/FadeAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Hack_Attack
+{
+    public class FadeAnimation : Animation
+    {
+        bool increase;
+        float fadeSpeed;
+        TimeSpan defaultTime, timer;
+        bool startTimer;
+        float activateValue;
+
+        public override void LoadContent(ContentManager Content, Texture2D image, string text, Vector2 position)
+        {
+            base.LoadContent(Content, image, text, position);
+            increase = false;
+            fadeSpeed = 1.0f;
+            defaultTime = new TimeSpan(0, 0, 1);
+            timer = defaultTime;
+            activateValue = 0.0f;
+        }
+    }
+}
