6c62df3e6b1aa05de037dfed5abc2a6eb697797c
Unknown <diogo_dpereira@hotmail.com>
2017-04-28 12:02:53 +0100

    15 MenuManager

---
+++ Hack Attack/Hack Attack/MenuManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Hack_Attack
+{
+    public class MenuManager
+    {
+        List<string> menuItems;
+        List<string> animationTypes;
+        List<Texture2D> menuImages;
+        List<List<Animation>> animation;
+
+        ContentManager content;
+
+        FileManager fileManager;
+
+        Vector2 position;
+        int axis;
+
+        List<List<string>> attributes, contents;
+
+        Rectangle source;
+
+        public void LoadContent(ContentManager content, string id)
+        {
+            this.content = new ContentManager(content.ServiceProvider, "Content");
+            menuItems = new List<string>();
+            animationTypes = new List<string>();
+            menuImages = new List<Texture2D>();
+            animation = new List<List<Animation>>();
+            attributes = new List<List<string>>();
+            contents = new List<List<string>>();
+
+            position = Vector2.Zero;
+
+            fileManager.LoadContent("Load/Menus.cme", attributes, contents, id);
+
+            for(int i = 0; i< attributes.Count; i++)
+            {
+                for(int j = 0; j < attributes[i].Count; j++)
+                {
+                    switch (attributes[i][j])
+                    {
+                        case "Item":
+                            menuItems.Add(contents[i][j]);
+                            break;
+
+                        case "Image":
+                            menuImages.Add(content.Load<Texture2D>(contents[i][j]));
+                            break;
+                        case "Axis":
+                            axis = int.Parse(contents[i][j]);
+                            break;
+                        case "Position":
+                            string[] temp = contents[i][j].Split(' ');
+                            position = new Vector2(float.Parse(temp[0]), float.Parse(temp[1]));
+                            break;
+                        case "Source":
+                            temp = contents[i][j].Split(' ');
+                            source = new Rectangle(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]), int.Parse(temp[3]));
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void UnloadContent()
+        {
+            content.Unload();
+            fileManager = null;
+            position = Vector2.Zero;
+            animation.Clear();
+            menuItems.Clear();
+            menuImages.Clear();
+            animationTypes.Clear();
+        }
+    }
+}
